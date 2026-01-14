bf1ea01ccb8b4c2a15dc8f38271e97f8f50db611
Luiz F Monge <luizfilipemonge@outlook.com>
2026-01-14 04:02:01 -0300

    feat: add TransferIndicator component to MainLayout for enhanced file transfer visibility

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import SideBarComponent from "@/components/SideBar";
 import NavBarComponent from "@/components/NavBar";
 import PageTransition from "@/components/PageTransition";
 import UploadManager from "@/components/UploadManager";
+import TransferIndicator from "@/components/TransferIndicator";
 import MobileDrawer from "@/components/MobileDrawer";
 
 export default function MainLayout({
@@ -12,6 +13,7 @@ export default function MainLayout({
     return (
         <>
             <UploadManager />
+            <TransferIndicator />
             <div className="grid-wrapper h-full">
                 <div className="grid-background"></div>
                 <div className="relative z-10 h-full p-4 flex gap-2">
